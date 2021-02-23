d8c667b4f5ce3709da41f46d5fbad5e987dbd10d
appu22 <appu.xworkz@gmail.com>
2021-02-23 08:52:40 +0530

    example of Inheritance

---
+++ opeartingsystem/src/com/xworkz/xworkzapp/os/Linux.java
@@ -0,0 +1,10 @@
+package com.xworkz.xworkzapp.os;
+
+public class Linux extends Os {
+	//public String bit;
+	//public String vesrion;
+	public void provideSecure()
+	{
+		System.out.println("Linux is Security os");
+	}
+}
---
+++ opeartingsystem/src/com/xworkz/xworkzapp/os/Os.java
@@ -0,0 +1,11 @@
+package com.xworkz.xworkzapp.os;
+
+public class Os {
+	public String bit;
+	public String vesrion;
+	public boolean openSource;
+	
+	public void runApp() {
+		System.out.println("Running Most Wanted application");
+}
+}
