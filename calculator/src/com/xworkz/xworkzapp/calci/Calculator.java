500f11bca4142f3915b6c8c3808b1b2fb28217a2
appu22 <appu.xworkz@gmail.com>
2021-02-25 10:59:33 +0530

    Example of inheritance Calculator

---
+++ calculator/src/com/xworkz/xworkzapp/calci/Calculator.java
@@ -0,0 +1,9 @@
+package com.xworkz.xworkzapp.calci;
+
+public class Calculator {
+	public void add(int a, int b) {
+		int c = a + b;
+		System.out.println("\n hii i am parent or super or base class");
+		System.out.println(" Adding two numbers " + c);
+	}
+}
---
+++ calculator/src/com/xworkz/xworkzapp/calci/Scientific.java
@@ -0,0 +1,16 @@
+package com.xworkz.xworkzapp.calci;
+
+//child class or derived class sub class
+public class Scientific extends Calculator {
+	String name;
+	int price;
+	public Scientific(String calName, int rs) {
+		System.out.println("i am constructor " + calName + "Price " + rs);
+	}
+	public void sub(int a, int b) {
+		int c = a - b;
+		System.out.println("\n hi i am child or derived or sub class ");
+		System.out.println("Substracting two numbers " + c);
+	}
+
+}
